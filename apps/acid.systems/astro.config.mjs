f88e6c928ced0042a5423d8a118e34c0fbad639f
Raúl Garcia <ego.war@icloud.com>
2026-05-27 06:01:02 -0700

    ⚡️ Lint forrmat
    frm lnt

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -3,13 +3,16 @@ import {defineConfig, fontProviders} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [{
-    provider: fontProviders.fontsource(),
-    name: 'Instrument Serif',
-    cssVariable: '--font-serif',
-  }, {
-    provider: fontProviders.fontsource(),
-    name: 'JetBrains Mono',
-    cssVariable: '--font-mono',
-  }]
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Instrument Serif',
+      cssVariable: '--font-serif',
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'JetBrains Mono',
+      cssVariable: '--font-mono',
+    },
+  ],
 })
