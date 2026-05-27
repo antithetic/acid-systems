bf7372fb02490fa4e95b55edbfaf396bf72ac2b8
Raúl Garcia <ego.war@icloud.com>
2026-05-26 20:14:01 -0700

    🎨 Fontsource Font API
    Used an Astro Font API provider to update the font system.

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import {defineConfig} from 'astro/config'
+import { fontProviders } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Instrument Serif",
+    cssVariable: "--font-serif",
+  }]
+})
