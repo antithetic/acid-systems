d5c5a4f6eaa87f65f982ccea6a9653e3c76bfab2
Raúl Garcia <ego.war@icloud.com>
2026-05-26 20:23:03 -0700

    🎨 Ignore files
    Individual ignore files for projects

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import {defineConfig} from 'astro/config'
-import { fontProviders } from 'astro/config'
+import {fontProviders} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [{
-    provider: fontProviders.fontsource(),
-    name: "Instrument Serif",
-    cssVariable: "--font-serif",
-  }]
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Instrument Serif',
+      cssVariable: '--font-serif',
+    },
+  ],
 })
