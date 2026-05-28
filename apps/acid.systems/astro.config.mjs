9a824689afb0db53066c5b1ca3e242549a762a75
Raúl Garcia <ego.war@icloud.com>
2026-05-27 19:14:50 -0700

    🎨 Lint
    Linty

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import {defineConfig, fontProviders} from 'astro/config'
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import sanity from '@sanity/astro'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,10 +19,13 @@ export default defineConfig({
     },
   ],
 
-  integrations: [sanity({
-    projectId: 'nmhp3u9m',
-    dataset: 'production',
-    apiVersion: '2026-05-27',
-    useCdn: false,
-  }), react()],
+  integrations: [
+    sanity({
+      projectId: 'nmhp3u9m',
+      dataset: 'production',
+      apiVersion: '2026-05-27',
+      useCdn: false,
+    }),
+    react(),
+  ],
 })
