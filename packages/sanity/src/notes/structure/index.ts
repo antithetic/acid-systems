e7911093fa3d9a899cf6f29833721e3d4ffd2bb0
Raúl Garcia <ego.war@icloud.com>
2026-06-02 16:09:05 -0700

    🎨 Notes structure icons
    icons for better content matching

--- packages/sanity/src/notes/structure/index.ts
+++ packages/sanity/src/notes/structure/index.ts
@@ -1,13 +1,13 @@
 import {type StructureBuilder, type StructureResolver} from 'sanity/structure'
-import {BrainCog} from 'lucide-react'
+import {Palette, Notebook} from 'lucide-react'
 
 export const notesStructure: StructureResolver = (S: StructureBuilder) =>
   S.list()
     .title('Notes')
     .items([
       S.documentTypeListItem('signal').title('Signals'),
-      S.documentTypeListItem('field-note').title('Field Notes'),
-      S.documentTypeListItem('colorNote').title('Colors'),
+      S.documentTypeListItem('field-note').title('Field Notes').icon(Notebook),
+      S.documentTypeListItem('colorNote').title('Colors').icon(Palette),
       S.divider(),
       S.documentTypeListItem('notesSettings').title('Settings'),
     ])
